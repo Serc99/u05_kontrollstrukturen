5bee05199cfd1252f0e9b9075eba99d4cd906f64
Sercan Yildiz <sercan.yz@outlook.de>
2025-10-07 05:58:32 -0700

    create new file for Hochhaus2 task

---
+++ auf5.07/Hochhaus2.java
@@ -0,0 +1,26 @@
+public class Hochhaus2
+{
+   public static void main(String[] args)
+   {
+      System.out.println("        /\\        ");
+      System.out.println("       /  \\       ");
+      System.out.println("      /    \\      ");
+      System.out.println("     /      \\     ");
+      System.out.println("    /        \\    ");
+      System.out.println("   /   (  )   \\   ");
+      System.out.println("  /            \\  ");
+      System.out.println(" /______________\\ ");
+      System.out.println("/|              |\\");
+
+      for(int i = 0; i < 4 ; i++)
+      {
+         System.out.println(" |  ___    ___  |");
+         System.out.println(" |  | |    | |  |");
+         System.out.println(" |  ---    ---  |");
+      }
+         System.out.println(" |  Hotel  ___  |");
+         System.out.println(" |  |   |  | |  |");
+         System.out.println(" |  |   |  ---  |");
+         System.out.println(" ================");
+   }
+}
