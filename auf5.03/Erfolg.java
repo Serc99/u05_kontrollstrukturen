fa3d71c51f9649bed8144913bd00526212993922
Sercan Yildiz <sercan.yz@outlook.de>
2025-10-07 04:53:39 -0700

    Create initial commit with Erfolg task.

---
+++ auf5.03/Erfolg.java
@@ -0,0 +1,21 @@
+// u05_kontrollstruktur
+// auf5.03
+
+public class Erfolg
+{
+   public static void main(String[] args)
+   {
+      int a;
+      a = 3;
+
+      System.out.println("Regeln fuer den erfolg:");
+      System.out.println();
+
+      for(int i = 1; i <= a; i++)
+      {
+         System.out.println("regel " + i + ": ");
+         System.out.println("ueben!");
+      }
+   }
+}
+
