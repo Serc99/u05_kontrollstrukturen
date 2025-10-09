71fe58c88bf29351b52828c9be176b0430762098
Sercan Yildiz <sercan.yz@outlook.de>
2025-10-09 01:04:58 -0700

    Create file with Hotel task

--- auf5.11/Hotel.java
+++ auf5.11/Hotel.java
@@ -5,27 +5,45 @@ public class Hotel
 {
    public static void main(String[] args)
    {
+      System.out.println("        /\\        ");
+      System.out.println("       /  \\       ");
+      System.out.println("      /    \\      ");
+      System.out.println("     /      \\     ");
+      System.out.println("    /        \\    ");
+      System.out.println("   /   (  )   \\   ");
+      System.out.println("  /            \\  ");
+      System.out.println(" /______________\\ ");
+      System.out.println("/|              |\\"); 
+
+      System.out.println(" |  ___    ___  |");
+      System.out.println(" |  | |    | |  |");
+      System.out.println(" |  ---    ---  |");    
+ 
       int sterne;
       sterne = 2;
 
       if(sterne == 1)
       {
-         System.out.println("Absteige");
+         System.out.println(" |Absteige ___  |");
       }
       
       if(sterne == 2)
       {
-         System.out.println("Gasthaus");
+         System.out.println(" |Gasthaus ___  |");
       }
 
       if(sterne == 3)
       {
-         System.out.println("Hotel");
+         System.out.println(" |  Hotel  ___  |");
       }
 
       if(sterne == 4)
       {
-         System.out.println("Palast");
+         System.out.println(" |  Palast ___  |");
       }
+
+      System.out.println(" |  |   |  | |  |");
+      System.out.println(" |  |   |  ---  |");
+      System.out.println(" ================");
    }
 }
