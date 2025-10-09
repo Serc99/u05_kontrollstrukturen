0081a9f5bbfb527907b96c7f3059dd187033f6f1
Sercan Yildiz <sercan.yz@outlook.de>
2025-10-09 00:56:17 -0700

    Create file with Hotel task

---
+++ auf5.11/Hotel.java
@@ -0,0 +1,31 @@
+// u05_kontrollstrukturen
+// auf5.11
+
+public class Hotel
+{
+   public static void main(String[] args)
+   {
+      int sterne;
+      sterne = 2;
+
+      if(sterne == 1)
+      {
+         System.out.println("Absteige");
+      }
+      
+      if(sterne == 2)
+      {
+         System.out.println("Gasthaus");
+      }
+
+      if(sterne == 3)
+      {
+         System.out.println("Hotel");
+      }
+
+      if(sterne == 4)
+      {
+         System.out.println("Palast");
+      }
+   }
+}
